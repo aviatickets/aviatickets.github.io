50c38525d6cfd291ee8b6e99dcf345f0dcbaf67a
aviatickets <sunspot-revers.0s@icloud.com>
2024-07-13 00:14:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ $("#dates_from").flatpickr({
             onChange: function(selectedDates, dateStr, instance) {
             if (selectedDates.length != 0) {
                 item.set("dates_from", dateStr);
-                if (!from_field.value && !to_field.value and item.has('dates_to')) {
+                if (!from_field.value && !to_field.value && item.has('dates_to')) {
                     tg.MainButton.setText("Поиск билетов");
 		            tg.MainButton.show(); } else {tg.MainButton.hide();}}
 		    else {
