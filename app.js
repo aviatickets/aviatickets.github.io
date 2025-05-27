aa3250155a771824eae82b9d05a429b44db6e60b
aviatickets <sunspot-revers.0s@icloud.com>
2025-05-27 12:52:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -32,6 +32,11 @@ function populateInputFields() {
     document.getElementById('noTransfersWithVisa').checked = urlParams['noTransfersWithVisa'];
     document.getElementById('noAirportChange').checked = urlParams['noAirportChange'];
     document.getElementById('noRecheck').checked = urlParams['noRecheck'];
+  if (urlParams['onlyWithLuggage']) {item.set("onlyWithLuggage", urlParams['onlyWithLuggage']);}
+  if (urlParams['noTransfersWithVisa']) {item.set("noTransfersWithVisa", urlParams['noTransfersWithVisa']);}
+  if (urlParams['noAirportChange']) {item.set("noAirportChange", urlParams['noAirportChange']);}
+  if (urlParams['noRecheck']) {item.set("noRecheck", urlParams['noRecheck']);}
+	
   if (urlParams['from_field']) {document.getElementById('from_field').value = urlParams['from_field'];
                                 item.set("from_field", urlParams['from_field']);}
   if (urlParams['from_code_field']) {item.set("from_code_field", urlParams['from_code_field']);}
